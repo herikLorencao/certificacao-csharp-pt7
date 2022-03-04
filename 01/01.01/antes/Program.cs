01014e07efeee24233c29ae8392499f56b5fa091
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-03-04 16:13:07 -0300

    adicionando introdução aos eventos

--- 01/01.01/antes/Program.cs
+++ 01/01.01/antes/Program.cs
@@ -6,6 +6,45 @@ namespace _01_01
     {
         static void Main(string[] args)
         {
+            Campainha campainha = new();
+            campainha.OnCampainhaTocou += TocarCampainha1;
+            campainha.OnCampainhaTocou += TocarCampainha2;
+            campainha.Tocar("101");
+            Console.WriteLine();
+
+            campainha.OnCampainhaTocou -= TocarCampainha1;
+            campainha.Tocar("202");
+        }
+
+        static void TocarCampainha1(object sender, CampainhaEventArgs args)
+        {
+            Console.WriteLine($"A campainha tocou no apartamento {args.Apartamento} (1)");
+        }
+
+        static void TocarCampainha2(object sender, CampainhaEventArgs args)
+        {
+            Console.WriteLine($"A campainha tocou no apartamento {args.Apartamento} (2)");
+        }
+    }
+
+    class Campainha
+    {
+        public event EventHandler<CampainhaEventArgs> OnCampainhaTocou;
+
+        public void Tocar(string apartamento)
+        {
+            if (OnCampainhaTocou != null)
+                OnCampainhaTocou(this, new CampainhaEventArgs(apartamento));
+        }
+    }
+
+    class CampainhaEventArgs : EventArgs
+    {
+        public string Apartamento { get; }
+
+        public CampainhaEventArgs(string apartamento)
+        {
+            Apartamento = apartamento;
         }
     }
 }
