e9a6426b7f543f37b19bf0f5d746a81c40c74fc9
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2022-03-04 17:43:36 -0300

    adicionando delegados e lambda

--- 01/01.03/antes/Program.cs
+++ 01/01.03/antes/Program.cs
@@ -4,9 +4,31 @@ namespace _01_03
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+            
         static void Main(string[] args)
         {
+            int a = 3;
+            int b = 2;
+
+            Operacao operacao = Somar;
+            Console.WriteLine(operacao(a, b));
+            operacao = Subtrair;
+            Console.WriteLine(operacao(a, b));
+            
             Console.ReadKey();
+        }
+
+        static int Somar(int a, int b)
+        {
+            Console.WriteLine($"Operação de somar a = {a} e b = {b}");
+            return a + b;
+        }
+        
+        static int Subtrair(int a, int b)
+        {
+            Console.WriteLine($"Operação de subtrair a = {a} e b = {b}");
+            return a - b;
         }
     }
 }
--- 01/01.04/antes/Program.cs
+++ 01/01.04/antes/Program.cs
@@ -4,8 +4,12 @@ namespace _01_04
 {
     class Program
     {
+        delegate int Operacao(int a, int b);
+            
         static void Main(string[] args)
         {
+            Operacao operacao = (x, y) => x + y;
+            Console.WriteLine(operacao(3, 2));
         }
     }
 }
